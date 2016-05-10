fbfe163cc7feaf9561b6c5153ceaf030d378ae78
tomhamiltonstubber <tomhamiltonstubber@gmail.com>
2016-05-10 12:08:54 +0100

    Moving is_raw logic to template

--- data.js
+++ data.js
@@ -1,10 +1,5 @@
 var data = {
   logo: 'tc-box-logo.png',
   client_name: 'Example',
-  client_street: '123 fake street',
+  client_street: '123 fake street'
 };
-
-if (window.location.href.indexOf('raw=true') === -1) {
-  console.log('render template');
-  document.body.innerHTML = Mustache.render(document.body.innerHTML, data);
-}
