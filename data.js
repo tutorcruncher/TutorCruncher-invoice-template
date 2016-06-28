fa5d2e5de4c9d8afed2f5dfaaff8d26016a43c44
Maxime Ansquer <maximeansquer@gmail.com>
2016-06-28 17:35:36 +0100

    all fields filled with examples

--- data.js
+++ data.js
@@ -1,5 +1,65 @@
 var data = {
+  invoice_label: 'Fake Invoice',
+
+  first_page_subtitle: 'Invoice INV-1234, Page 1 of 2',
+  date_sent: '28/06/2016',
+
   logo: 'tc-box-logo.png',
-  client_name: 'Example',
-  client_street: '123 fake street'
+
+  client_name: 'John Doe',
+  client_street: '123 Example Street',
+  client_town: 'London',
+  client_country: 'United Kingdom',
+  client_postcode: 'WC2A 3BP',
+  client_phone: '071234 12345',
+  client_email: 'john.doe@example.com',
+
+  branch_name: 'Dinosaur Tutors',
+  branch_street: '8A Lower Grosvenor Place, Victoria',
+  branch_town: 'London',
+  branch_country: 'United Kingdom',
+  branch_postcode: 'SW1W 0EN',
+  branch_email: 'info@tutorcruncher.com',
+
+  branch_statement_prefix: 'We would ask you to settle the invoice within 7 days.',
+
+  has_charges_via_contractor: false,
+  has_charges_via_branch:true,
+
+  total_charged_via_branch: 120,
+  invoice_amount: 120,
+
+  please_quote: 'Please quote the invoice reference with your payment.',
+
+  branch_payment_details: 'Dinosaur Tutors<br/>' +
+                          'Account Number - 12345678<br/>' +
+                          'Sort Code - 11-22-33<br/>' +
+                          'NatWest Bank 141 Ebury St<br/>' +
+                          'London<br/>' +
+                          'SW1W 9QW',
+
+  page_subtitle: 'Invoice INV-1234, Page 2 of 2',
+
+  payee_is_branch: true,
+  payee_website: 'https://tutorcruncher.com',
+  payee_email: 'info@tutorcruncher.com',
+
+  items: true,
+
+  date: '28/06/2016 17:20',
+
+  description:'Maths',
+  reference: 'Job #6',
+  cancelled: false,
+  contractor_label: 'Tutor',
+  contractors: 'Brain Johnston',
+
+  units: '1 hour',
+
+  amount: 100,
+
+  net_amount: 100,
+  vat_rate: 20,
+  tax_amount: 20,
+  gross_amount: 120,
 };
